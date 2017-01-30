ec84522a459a37de8e98bdebdc3a199c8d2d4fa7
Frank Hunleth <fhunleth@troodon-software.com>
2017-01-30 10:42:41 -0500

    Various fixes to sdburner

--- package/sdburner/sdburner.mk
+++ package/sdburner/sdburner.mk
@@ -4,10 +4,10 @@
 #
 ################################################################################
 
-SDBURNER_VERSION = ca86d3ed3f331ba6146df71a6020c91aa28d45c9
-#SDBURNER_SITE = $(call github,fhunleth,sdburner,$(SDBURNER_VERSION))
-SDBURNER_SITE = /home/fhunleth/experiments/sdburner
-SDBURNER_SITE_METHOD = local
+SDBURNER_VERSION = 6e58a9da0b94979e761af787da3c26a679abdb6a
+SDBURNER_SITE = $(call github,fhunleth,sdburner,$(SDBURNER_VERSION))
+#SDBURNER_SITE = /home/fhunleth/experiments/sdburner
+#SDBURNER_SITE_METHOD = local
 SDBURNER_LICENSE = Apache2
 SDBURNER_DEPENDENCIES = qt5base fwup
 
