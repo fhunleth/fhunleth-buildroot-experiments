545b8216a429b30e263ad69a5df39de28d352370
Frank Hunleth <fhunleth@troodon-software.com>
2018-06-01 17:15:46 -0400

    Create test image for net configuration

---
+++ board/rpi3_nettest/README.md
@@ -0,0 +1,3 @@
+# Raspberry Pi 3 B/B+ Network test image
+
+
---
+++ board/rpi3_nettest/post-createfs.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+
+TARGETDIR=$1
+FWNAME=$2
+
+if [[ -z $FWNAME ]]; then
+    FWNAME=rpi3_nettest-$(git -C $BR2_EXTERNAL_FHUNLETH_PATH describe --dirty)
+fi
+
+FWUP_CONFIG=$BR2_EXTERNAL_FHUNLETH_PATH/board/rpi3_nettest/fwup.conf
+FWUP=$HOST_DIR/usr/bin/fwup
+
+FW_PATH=$BINARIES_DIR/$FWNAME.fw
+
+# Build the firmware image (.fw file)
+echo "Creating $FWNAME.fw..."
+$FWUP -c -f $FWUP_CONFIG -o $FW_PATH
+
