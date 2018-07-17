9945f4b94e2db6944ac90d908154fa588e537a54
Frank Hunleth <fhunleth@troodon-software.com>
2018-07-17 16:02:52 -0400

    More rpi2 SDCard burner fixes

--- package/sdburner/sdburner.mk
+++ package/sdburner/sdburner.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-SDBURNER_VERSION = 6e58a9da0b94979e761af787da3c26a679abdb6a
+SDBURNER_VERSION = v0.2.1
 SDBURNER_SITE = $(call github,fhunleth,sdburner,$(SDBURNER_VERSION))
 #SDBURNER_SITE = /home/fhunleth/experiments/sdburner
 #SDBURNER_SITE_METHOD = local
